3e023e70fbde70e418c19fa441fd51f5ba5b2226
Jafar610 <jafarmohamed955@gmail.com>
2026-08-07 23:13:11 +0300

    header setup

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <span className="border-top"></span>
+        <h2>Dwello</h2>
+      </div>
+
+      <div>
+        <a href="">Home</a>
+        <a href="">Service</a>
+        <a href="">Agent</a>
+        <a href="">Contact</a>
+      </div>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import hero from 'https://hyderabadresidentialprojects.com/projects-in-kondapur/auro-sapphire/assets/images/auro21.webp'
+function Hero() {
+  return (
+    <>
+    <div>
+        <div></div>
+
+        <div>
+           <img src={hero} alt="hero" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
