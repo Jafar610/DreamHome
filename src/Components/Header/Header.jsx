c4a992c6f5c3bff5190c73be74545069b3ae76ef
Jafar610 <jafarmohamed955@gmail.com>
2026-08-19 13:57:27 +0300

    header completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,33 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import PersonIcon from "@mui/icons-material/Person";
 function Header() {
+  const [input, setInput] = useState("hidden");
+ const handleClicked = () => {
+  setInput(prev => prev === "hidden" ? "visible" : "hidden");
+};
   return (
-    <div className="flex justify-between align-center bg-[#fef7f2] p-5">
-      <div className="border-top-2 border-[#f15a29]">
-        <span className="border-top"></span>
-        <h2>Dwello</h2>
-      </div>
-
-      <div className="flex justify-center align-center gap-5">
-        <a href="">Home</a>
-        <a href="">Service</a>
-        <a href="">Agent</a>
-        <a href="">Contact</a>
-      </div>
-
-      <div className="flex justify-center align-center gap-5">
-        <div>
-          <input type="text" placeholder="Search" className="hidden" />
-          <SearchIcon />
-        </div>
-        <div>
-          <PersonIcon />
+    <>
+      <div className="flex justify-between align-center bg-[#FEF7F2] px-20 py-6">
+        <div className="border-top-2 border-[#2A1B12]">
+          <span className="border-top"></span>
+          <h2 className="font-bold text-lg">Dwello</h2>
         </div>
 
-        <button>Sign up</button>
+        <div className="flex justify-center gap-10 text-lg text-[#2A1B12] font-bold ">
+          <a href="">Home</a>
+          <a href="">Service</a>
+          <a href="">Agent</a>
+          <a href="">Contact</a>
+        </div>
+
+        <div className="flex justify-center align-center gap-6">
+          <div
+            className="flex justify-center align-center py-1 border border-[#2A1B12] rounded-sm"
+            onClick={handleClicked}
+          >
+            <input
+              type="text"
+              placeholder="Search"
+              className={`${input} outline-none px-2`}
+            />
+            <SearchIcon />
+          </div>
+          <div className="flex justify-center align-center text-lg py-1">
+            <PersonIcon />
+          </div>
+
+          <div className="flex align-center border rounded-lg px-4 py-1 bg-[#2A1B12] text-white">
+            <button className="text-center">Sign up</button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
